cfb3641fafd8c905c836596186d7f282ab39e792
Mehdi Touhami <touhamimehdigpt2@gmail.com>
2026-05-15 21:53:36 +0100

    Update backend URL to new Railway domain

--- sleepapp/lib/core/constants/app_config.dart
+++ sleepapp/lib/core/constants/app_config.dart
@@ -2,5 +2,5 @@
 /// Local dev:  'http://localhost:8000'
 /// Deployed:   'https://your-app.up.railway.app'
 class AppConfig {
-  static const String kBaseUrl = 'https://web-production-a9ef4.up.railway.app';
+  static const String kBaseUrl = 'https://itri-sleep-app-production.up.railway.app';
 }
